e8d80d79ee947804f3675fdc1813c5201ce2cd25
jjenkins120 <jjenkins120@gmail.com>
2020-12-23 12:28:32 -0600

    added sign in screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import Signin from './src/screens/SigninScreen'
+import SigninScreen from './src/screens/SigninScreen'
 
 const navigator = createStackNavigator(
   {
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,18 +1,57 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Text, Button, Input } from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
-const Signin = () => {
-    return <>
-        <Text>Welcome to Auth Practice</Text>
-        <Input placeholder='Name'/>
-        <Input placeholder='Password'/>
-        <Button title='Sign In'/>
-    </>
+const SigninScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Spacer>
+                <Text style={styles.header}>Welcome to Auth Practice</Text>
+            </Spacer>
+            <Spacer>
+                <Input placeholder='Name'/>
+            </Spacer>
+            <Spacer>
+                <Input placeholder='Password'/>
+            </Spacer>
+            <Spacer>
+                <Button 
+                    title='Sign In'
+                    raised
+                    type=''
+                    containerStyle={styles.button}
+                    titleStyle={styles.title}
+                />
+            </Spacer>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
+SigninScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+};
 
+const styles = StyleSheet.create({
+    header: {
+        textAlign: 'center',
+        fontSize: 25, 
+        fontWeight: 'bold',
+        color: '#8A2BE2',
+
+    },
+    screen: {
+        marginTop: 100,
+        flex: 1
+    },
+    button: {
+        marginHorizontal: 9,
+    },
+    title: {
+        color:'#8A2BE2'
+    }
 })
 
-export default Signin
+export default SigninScreen
